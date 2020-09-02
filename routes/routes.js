7a938bf7462dc0d1d8e1544d5b0a68b7a6b7e902
James <james.hudgins.work@gmail.com>
2020-09-02 17:54:06 -0400

    changed arch to ex-hb with bulma css

---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+// Dependencies START
+// *********************************************************************************
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+var db = require("../models");
+const projects = require("../models/project");
+// *********************************************************************************
+// Dependencies END
+
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+
+// Routes START
+router.get("/", function(req, res) {
+  // Query posts for all posts
+  db.Project.findAll({
+    // Add order conditions here....
+  }).then(function (projectData) {
+    console.log(projectData)
+    // create handle bars obj to be rendered
+    var xPrs_HB_DATA = {
+      entireRequest: req,
+      requestBody: req.body,
+      Project: projectData,
+    }
+    res.render("index", xPrs_HB_DATA)
+  
+  });
+
+
+});
+
+
+
+module.exports = router;
